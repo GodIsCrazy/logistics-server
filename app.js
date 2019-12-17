ef877befc9e106a648840eb4744bce6f9b2be1a5
messyalone <506525306@qq.com>
2019-12-17 13:28:46 +0800

    login

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-//解析客户端请求中的body内容，内容用JSON
-var bodyParser = require('body-parser');
+var tokenUtil =require('./util/tokenUtil.js')
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,14 +16,30 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-//加载json解析中间件，接收json请求
-app.use(bodyParser.json());
-//加载解析url encode中间件
-app.use(bodyParser.urlencoded({extended: false }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+//全局拦截
+app.use(function (req, res, next) {
+
+  if (req.url != '/users/login1' && req.url != '/users/register'&& req.url != '/') {
+    let token = req.headers.token;
+    if (!tokenUtil.checkToken(token)) {
+      console.log("token失效！");
+      res.send({status: 403, msg: '登录已过期,请重新登录'});
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var db=require('../mysql.js');
-
+var bcrypt = require('bcryptjs')
+var salt = bcrypt.genSaltSync(10);
+var tokenUtil =require('../util/tokenUtil.js')
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/login', function(req, res, next) {
+  res.render("layout")
+
+});
+
+router.get('/list', function(req, res, next) {
+  console.log("test！！！")
+  res.render("layout")
 
 });
 
@@ -12,25 +20,32 @@ router.get('/', function(req, res, next) {
 router.post('/login', function(req, res, next) {
   // res.send('respond with a resource');
   // 查询实例
-
+  console.log(111111111111)
 // 获取参数
-  var param = [req.body.userName, req.body.userPwd];
-  console.log(req.body);
-  var findOneSql = 'select * from t_user where user_name = ? and user_pwd=?';
-  db.query(findOneSql, param,function(result,fields){
-    console.log(result)
+  var param = [, ];
+  var pass = req.body.userPwd;
+  var findOneSql = 'select * from sys_user where login_name = ? ';
+  db.query(findOneSql, req.body.userName,function(result,fields){
     if(result.length>0){
-      res.cookie("userId",result.userId,{
-        path:'/',
-        maxAge:100*60*60
-      });
-      res.json({
-        status:"0",
-        msg:'',
-        result:{
-          userName:result.userName
-        }
-      })
+      var userPwd = result[0].password;
+      if(bcrypt.compare(pass, userPwd)){
+        var token = tokenUtil.createToken(result[0].user_name,60*30);
+        console.log(token)
+        res.json({
+          status:"0",
+          msg:'',
+          result:{
+            userName:result[0].user_name,
+            token:token
+          }
+        });
+      }else{
+        res.json({
+          status:"1",
+          msg:'账号名或密码错误！'
+        })
+      }
+
     }else{
       res.json({
         status:"1",
