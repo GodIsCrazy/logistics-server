a80d557c7c519f2c88c440d0a1b1f4c210d64ae0
messyalone <506525306@qq.com>
2019-12-19 10:41:07 +0800

    src - logistics

--- app.js
+++ app.js
@@ -44,8 +44,8 @@ app.use(function (req, res, next) {
   }
 });
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/logistics', indexRouter);
+app.use('/logistics/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
